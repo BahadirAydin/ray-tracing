6f5f250c7edfad379ce0b1a53d185765fcce3761
BahadirAydin <bahadiraydin1923@gmail.com>
2023-10-26 20:17:49 +0300

    slm

--- Ray.cpp
+++ Ray.cpp
@@ -1,44 +1,35 @@
+#include "Ray.h"
 #include "parser.h"
 #include "utils.cpp"
 
-class Ray {
-public:
-    parser::Vec3f origin;
-    parser::Vec3f direction;
-};
+Ray generate_ray(const parser::Camera &camera, int i, int j, float pixel_width,
+                 float pixel_height) {
 
-Ray generate_ray(const parser::Camera& camera, int i, int j, float pixel_width, float pixel_height) {
-    // Extracting values from the Camera object for clarity
-    parser::Vec3f e = camera.position;
-    parser::Vec3f gaze = camera.gaze;
-    parser::Vec3f up = camera.up;
-    parser::Vec4f near_plane = camera.near_plane;
-    float distance = camera.near_distance;
-    
-    // Compute the w, u, v vectors
-    parser::Vec3f w = {-gaze.x, -gaze.y, -gaze.z}; 
-    parser::Vec3f u = cross_product(up, w);
-    parser::Vec3f v = {w.y * u.z - w.z * u.y, w.z * u.x - w.x * u.z, w.x * u.y - w.y * u.x}; 
+  parser::Vec3f e = camera.position;
+  parser::Vec3f gaze = camera.gaze;
+  parser::Vec3f up = camera.up;
+  parser::Vec4f near_plane = camera.near_plane;
+  float distance = camera.near_distance;
 
-    // Calculate su and sv
-    float su =  (i + 0.5) * pixel_width;
-    float sv =  (j + 0.5) * pixel_height;
+  parser::Vec3f w = {-gaze.x, -gaze.y, -gaze.z};
+  parser::Vec3f u = cross_product(up, w);
+  parser::Vec3f v = {w.y * u.z - w.z * u.y, w.z * u.x - w.x * u.z,
+                     w.x * u.y - w.y * u.x};
 
-    // Calculate q
-    parser::Vec3f m = {e.x + gaze.x * distance, e.y + gaze.y * distance, e.z + gaze.z * distance}; 
-    parser::Vec3f q = {
-        m.x + near_plane.x * u.x + up.x * near_plane.w,
-        m.y + near_plane.x * u.y + up.y * near_plane.w,
-        m.z + near_plane.x * u.z + up.z * near_plane.w
-    };
+  float su = (i + 0.5) * pixel_width;
+  float sv = (j + 0.5) * pixel_height;
 
-    // Calculate the direction d from e to q
-    parser::Vec3f d = {q.x - e.x, q.y - e.y, q.z - e.z};
+  parser::Vec3f m = add_vectors(e, multiply_vector(w, distance));
 
-    // Construct the Ray
-    Ray ray;
-    ray.origin = e;
-    ray.direction = d;
+  parser::Vec3f q =
+      add_vectors(m, add_vectors(multiply_vector(u, near_plane.x),
+                                 multiply_vector(v, near_plane.w)));
 
-    return ray;
+  parser::Vec3f s = add_vectors(
+      q, add_vectors(multiply_vector(u, su), multiply_vector(v, -1 * sv)));
+
+  Ray ray;
+  ray.set_origin(e);
+  parser::Vec3f direction = add_vectors(s, multiply_vector(e, -1));
+  return ray;
 }
--- Ray.h
+++ Ray.h
@@ -2,15 +2,13 @@
 
 class Ray {
 public:
-    void set_origin(const parser::Vec3f &origin) {
-        this->origin = origin;
-    }
-    void set_direction(const parser::Vec3f &direction) {
-        this->direction = direction;
-    }
+  void set_origin(const parser::Vec3f &origin) { this->origin = origin; }
+
+  void set_direction(const parser::Vec3f &direction) {
+    this->direction = direction;
+  }
+
 private:
   parser::Vec3f origin;
   parser::Vec3f direction;
 };
-
-
