31323c299d4aa3bda5bde47d5936c0b1d6c73764
BahadirAydin <bahadiraydin1923@gmail.com>
2023-10-26 19:25:18 +0300

    commit

---
+++ Ray.cpp
@@ -0,0 +1,5 @@
+#include "Ray.h"
+
+Ray generate_ray(const parser::Camera& camera, int i, int j) {
+
+}
---
+++ Ray.h
@@ -0,0 +1,8 @@
+#include "parser.h"
+
+class Ray {
+public:
+private:
+  parser::Vec3f origin;
+  parser::Vec3f direction;
+};
