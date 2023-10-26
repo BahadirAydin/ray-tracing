90caddddbb2a0937fc8bd2d9424db41ca4823819
BahadirAydin <bahadiraydin1923@gmail.com>
2023-10-26 20:52:08 +0300

    31

--- utils.h
+++ utils.h
@@ -1,3 +1,6 @@
+
+#ifndef UTILS_H
+#define UTILS_H
 #include "parser.h"
 #include <cmath>
 
@@ -26,3 +29,4 @@ void normalize(parser::Vec3f &vec) {
   vec.y /= magnitude;
   vec.z /= magnitude;
 }
+#endif // UTILS_H
