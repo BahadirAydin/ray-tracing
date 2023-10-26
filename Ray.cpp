2a79fb8844d42ae485d08b783878eeb0269fbc9f
DENIZCAN YILMAZ <e2444172@ceng.metu.edu.tr>
2023-10-26 20:05:19 +0300

    ray generator

--- Ray.cpp
+++ Ray.cpp
@@ -1,31 +1,44 @@
-#include "Ray.h"
+#include "parser.h"
 #include "utils.cpp"
 
-Ray generate_ray(const parser::Camera& camera, int i, int j) {
-    Ray ray;
-    ray.set_origin(camera.position);
-
-    parser::Vec3f right_vector = cross_product(camera.gaze, camera.up);
-    // normalize(right_vector);
-
-    parser::Vec3f up_vector = cross_product(right_vector, camera.gaze);
-    // normalize(up_vector);
-
-    float l = camera.near_plane.x;
-    float r = camera.near_plane.y;
-    float b = camera.near_plane.z;
-    float t = camera.near_plane.w;
-    
-    float u = l + (r - l) * (j + 0.5) / camera.image_width;
-    float v = b + (t - b) * (i + 0.5) / camera.image_height;
-
+class Ray {
+public:
+    parser::Vec3f origin;
     parser::Vec3f direction;
-    direction.x = camera.gaze.x * camera.near_distance + right_vector.x * u + up_vector.x * v;
-    direction.y = camera.gaze.y * camera.near_distance + right_vector.y * u + up_vector.y * v;
-    direction.z = camera.gaze.z * camera.near_distance + right_vector.z * u + up_vector.z * v;
+};
 
-    // normalize(direction);
-    ray.set_direction(direction);
+Ray generate_ray(const parser::Camera& camera, int i, int j, float pixel_width, float pixel_height) {
+    // Extracting values from the Camera object for clarity
+    parser::Vec3f e = camera.position;
+    parser::Vec3f gaze = camera.gaze;
+    parser::Vec3f up = camera.up;
+    parser::Vec4f near_plane = camera.near_plane;
+    float distance = camera.near_distance;
+    
+    // Compute the w, u, v vectors
+    parser::Vec3f w = {-gaze.x, -gaze.y, -gaze.z}; 
+    parser::Vec3f u = cross_product(up, w);
+    parser::Vec3f v = {w.y * u.z - w.z * u.y, w.z * u.x - w.x * u.z, w.x * u.y - w.y * u.x}; 
+
+    // Calculate su and sv
+    float su =  (i + 0.5) * pixel_width;
+    float sv =  (j + 0.5) * pixel_height;
+
+    // Calculate q
+    parser::Vec3f m = {e.x + gaze.x * distance, e.y + gaze.y * distance, e.z + gaze.z * distance}; 
+    parser::Vec3f q = {
+        m.x + near_plane.x * u.x + up.x * near_plane.w,
+        m.y + near_plane.x * u.y + up.y * near_plane.w,
+        m.z + near_plane.x * u.z + up.z * near_plane.w
+    };
+
+    // Calculate the direction d from e to q
+    parser::Vec3f d = {q.x - e.x, q.y - e.y, q.z - e.z};
+
+    // Construct the Ray
+    Ray ray;
+    ray.origin = e;
+    ray.direction = d;
 
     return ray;
 }
