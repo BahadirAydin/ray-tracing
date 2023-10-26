1c9b087f1ae9820cb84e06d1be35f7c8eb91dc4e
DENIZCAN YILMAZ <e2444172@ceng.metu.edu.tr>
2023-10-26 20:24:32 +0300

    rayy

--- Ray.cpp
+++ Ray.cpp
@@ -7,19 +7,17 @@ Ray generate_ray(const parser::Camera &camera, int i, int j, float pixel_width,
 
   parser::Vec3f e = camera.position;
   parser::Vec3f gaze = camera.gaze;
-  parser::Vec3f up = camera.up;
+  parser::Vec3f v = camera.up;
   parser::Vec4f near_plane = camera.near_plane;
   float distance = camera.near_distance;
 
   parser::Vec3f w = {-gaze.x, -gaze.y, -gaze.z};
-  parser::Vec3f u = cross_product(up, w);
-  parser::Vec3f v = {w.y * u.z - w.z * u.y, w.z * u.x - w.x * u.z,
-                     w.x * u.y - w.y * u.x};
+  parser::Vec3f u = cross_product(v, w);
 
   float su = (i + 0.5) * pixel_width;
   float sv = (j + 0.5) * pixel_height;
 
-  parser::Vec3f m = add_vectors(e, multiply_vector(w, distance));
+  parser::Vec3f m = add_vectors(e, multiply_vector(gaze, distance));
 
   parser::Vec3f q =
       add_vectors(m, add_vectors(multiply_vector(u, near_plane.x),
