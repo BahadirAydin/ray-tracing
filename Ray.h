8c8cfac2c3f3c99dca5b7d357ae5de29fb0cc6e8
DENIZCAN YILMAZ <e2444172@ceng.metu.edu.tr>
2023-10-30 02:12:50 +0300

    add depth field and format code

--- Ray.h
+++ Ray.h
@@ -2,55 +2,71 @@
 #define RAY_H
 
 #include "parser.h"
+
 #include "utils.h"
+
 #include <iostream>
 
 class Ray {
-  public:
-    void set_origin(const parser::Vec3f &origin) { this->origin = origin; }
-    void set_direction(const parser::Vec3f &direction) {
-        this->direction = direction;
-    }
-    parser::Vec3f get_origin() const { return origin; }
-    parser::Vec3f get_direction() const { return direction; }
+   public: void set_origin(const parser::Vec3f & origin) {
+      this -> origin = origin;
+   }
+   void set_direction(const parser::Vec3f & direction) {
+      this -> direction = direction;
+   }
+   parser::Vec3f get_origin() const {
+      return origin;
+   }
+   parser::Vec3f get_direction() const {
+      return direction;
+   }
 
-    void print() {
-        std::cout << "origin: " << origin.x << " " << origin.y << " "
-                  << origin.z << " "
-                  << "direction: " << direction.x << " " << direction.y << " "
-                  << direction.z << std::endl;
-    }
+   void print() {
+      std::cout << "origin: " << origin.x << " " << origin.y << " " <<
+         origin.z << " " <<
+         "direction: " << direction.x << " " << direction.y << " " <<
+         direction.z << std::endl;
+   }
 
-  private:
-    parser::Vec3f origin;
-    parser::Vec3f direction;
+   int get_depth() {
+      return depth;
+   }
+
+   int inc_depth(int val) {
+      this -> depth += 1;
+      return this -> depth;
+   }
+
+   private: parser::Vec3f origin;
+   parser::Vec3f direction;
+   int depth = 0;
 };
 
-inline Ray generate_ray(const parser::Camera &camera, int i, int j,
-                        float pixel_width, float pixel_height) {
+inline Ray generate_ray(const parser::Camera & camera, int i, int j,
+   float pixel_width, float pixel_height) {
 
-    parser::Vec3f e = camera.position;
-    parser::Vec3f gaze = camera.gaze;
-    parser::Vec3f v = camera.up;
-    parser::Vec3f w = camera.w;
-    parser::Vec3f u = camera.u;
-    parser::Vec3f q = camera.q;
-    parser::Vec4f near_plane = camera.near_plane;
-    parser::Vec3f m = camera.plane_center;
-    float distance = camera.near_distance;
+   parser::Vec3f e = camera.position;
+   parser::Vec3f gaze = camera.gaze;
+   parser::Vec3f v = camera.up;
+   parser::Vec3f w = camera.w;
+   parser::Vec3f u = camera.u;
+   parser::Vec3f q = camera.q;
+   parser::Vec4f near_plane = camera.near_plane;
+   parser::Vec3f m = camera.plane_center;
+   float distance = camera.near_distance;
 
-    float su = (i + 0.5) * pixel_width;
-    float sv = (j + 0.5) * pixel_height;
+   float su = (i + 0.5) * pixel_width;
+   float sv = (j + 0.5) * pixel_height;
 
-    parser::Vec3f s = add_vectors(
-        q, add_vectors(multiply_vector(u, su), multiply_vector(v, -1 * sv)));
+   parser::Vec3f s = add_vectors(
+      q, add_vectors(multiply_vector(u, su), multiply_vector(v, -1 * sv)));
 
-    Ray ray;
-    ray.set_origin(e);
-    parser::Vec3f direction = subtract_vectors(s, e);
+   Ray ray;
+   ray.set_origin(e);
+   parser::Vec3f direction = subtract_vectors(s, e);
 
-    ray.set_direction(direction);
+   ray.set_direction(direction);
 
-    return ray;
+   return ray;
 }
 #endif // RAY_H
--- raytracer.cpp
+++ raytracer.cpp
@@ -1,62 +1,65 @@
 #include "Ray.h"
+
 #include "intersect.h"
+
 #include "parser.h"
+
 #include "ppm.h"
 
 typedef unsigned char RGB[3];
 
-int main(int argc, char *argv[]) {
-    // Sample usage for reading an XML scene file
-    parser::Scene scene;
+int main(int argc, char * argv[]) {
+   // Sample usage for reading an XML scene file
+   parser::Scene scene;
 
-    scene.loadFromXml(argv[1]);
+   scene.loadFromXml(argv[1]);
 
-    for (parser::Camera cam : scene.cameras) {
-        int nx = cam.image_width;
-        int ny = cam.image_height;
-        unsigned char *image = new unsigned char[nx * ny * 3];
-        // x y z w => l r b t
+   for (parser::Camera cam: scene.cameras) {
+      int nx = cam.image_width;
+      int ny = cam.image_height;
+      unsigned char * image = new unsigned char[nx * ny * 3];
+      // x y z w => l r b t
 
-        int i = 0;
-        float pixel_width = (cam.near_plane.y - cam.near_plane.x) / nx;
-        float pixel_height = (cam.near_plane.w - cam.near_plane.z) / ny;
-        for (int y = 0; y < ny; ++y) {
-            for (int x = 0; x < nx; ++x) {
-                Ray r = generate_ray(cam, x, y, pixel_width, pixel_height);
-                for (parser::Sphere sphere : scene.spheres) {
-                    float t = intersect_sphere(
-                        scene.vertex_data[sphere.center_vertex_id - 1],
-                        sphere.radius, r);
+      int i = 0;
+      float pixel_width = (cam.near_plane.y - cam.near_plane.x) / nx;
+      float pixel_height = (cam.near_plane.w - cam.near_plane.z) / ny;
+      for (int y = 0; y < ny; ++y) {
+         for (int x = 0; x < nx; ++x) {
+            Ray r = generate_ray(cam, x, y, pixel_width, pixel_height);
+            for (parser::Sphere sphere: scene.spheres) {
+               float t = intersect_sphere(
+                  scene.vertex_data[sphere.center_vertex_id - 1],
+                  sphere.radius, r);
 
-                    if (t > 0) {
-                        parser::Material material =
-                            scene.materials[sphere.material_id - 1];
+               if (t > 0) {
+                  parser::Material material =
+                     scene.materials[sphere.material_id - 1];
 
-                        // Calculate the intersection point
-                        parser::Vec3f intersection_point =
-                            add_vectors(r.get_origin(),
-                                        multiply_vector(r.get_direction(), t));
+                  // Calculate the intersection point
+                  parser::Vec3f intersection_point =
+                     add_vectors(r.get_origin(),
+                        multiply_vector(r.get_direction(), t));
 
-                        parser::Vec3f sphere_diffuse_color = material.diffuse;
-                        image[i++] = static_cast<unsigned char>(
-                            sphere_diffuse_color.x * 255);
-                        image[i++] = static_cast<unsigned char>(
-                            sphere_diffuse_color.y * 255);
-                        image[i++] = static_cast<unsigned char>(
-                            sphere_diffuse_color.z * 255);
-                    } else {
-                        // No intersection with the sphere, set the background
-                        // color
-                        image[i++] = static_cast<unsigned char>(
-                            scene.background_color.x * 255);
-                        image[i++] = static_cast<unsigned char>(
-                            scene.background_color.y * 255);
-                        image[i++] = static_cast<unsigned char>(
-                            scene.background_color.z * 255);
-                    }
-                }
+                  parser::Vec3f sphere_diffuse_color = material.diffuse;
+                  image[i++] = static_cast < unsigned char > (
+                     sphere_diffuse_color.x * 255);
+                  image[i++] = static_cast < unsigned char > (
+                     sphere_diffuse_color.y * 255);
+                  image[i++] = static_cast < unsigned char > (
+                     sphere_diffuse_color.z * 255);
+               } else {
+                  // No intersection with the sphere, set the background
+                  // color
+                  image[i++] = static_cast < unsigned char > (
+                     scene.background_color.x * 255);
+                  image[i++] = static_cast < unsigned char > (
+                     scene.background_color.y * 255);
+                  image[i++] = static_cast < unsigned char > (
+                     scene.background_color.z * 255);
+               }
             }
-        }
-        write_ppm("simple.ppm", image, nx, ny);
-    }
+         }
+      }
+      write_ppm("simple.ppm", image, nx, ny);
+   }
 }
