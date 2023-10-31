a7f8cf09579dda7c9063fc481a43581b0820a3dc
BahadirAydin <bahadiraydin1923@gmail.com>
2023-10-31 17:34:27 +0300

    color computations

---
+++ color.h
@@ -0,0 +1,103 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "intersect.h"
+#include "parser.h"
+#include "utils.h"
+
+inline parser::Vec3f compute_color(const parser::Scene &scene,
+                                   const Intersection &intersection) {
+    parser::Vec3f color = {0, 0, 0};
+
+    // Ambient lighting component
+    parser::Vec3f ambient_color = {
+        scene.ambient_light.x * intersection.material.ambient.x,
+        scene.ambient_light.y * intersection.material.ambient.y,
+        scene.ambient_light.z * intersection.material.ambient.z};
+
+    color.x += ambient_color.x;
+    color.y += ambient_color.y;
+    color.z += ambient_color.z;
+
+    for (const parser::PointLight &light : scene.point_lights) {
+        parser::Vec3f to_light = {light.position.x - intersection.point.x,
+                                  light.position.y - intersection.point.y,
+                                  light.position.z - intersection.point.z};
+        float distance_to_light =
+            sqrt(to_light.x * to_light.x + to_light.y * to_light.y +
+                 to_light.z * to_light.z);
+
+        if (distance_to_light > 0) {
+            to_light.x /= distance_to_light;
+            to_light.y /= distance_to_light;
+            to_light.z /= distance_to_light;
+
+            float d = dot_product(to_light, intersection.normal);
+
+            if (d > 0) {
+                parser::Vec3f diffuse_color = {
+                    intersection.material.diffuse.x * light.intensity.x * d,
+                    intersection.material.diffuse.y * light.intensity.y * d,
+                    intersection.material.diffuse.z * light.intensity.z * d};
+
+                color.x += diffuse_color.x;
+                color.y += diffuse_color.y;
+                color.z += diffuse_color.z;
+            }
+        }
+    }
+
+    color.x = std::max(0.0f, std::min(255.0f, color.x));
+    color.y = std::max(0.0f, std::min(255.0f, color.y));
+    color.z = std::max(0.0f, std::min(255.0f, color.z));
+
+    return color;
+}
+
+inline parser::Vec3f compute_background_color(const parser::Vec3f &pixel,
+                                              const parser::Scene &scene) {
+    parser::Vec3f color = {0, 0, 0};
+
+    parser::Vec3f ambient_color = {
+        scene.ambient_light.x * scene.background_color.x,
+        scene.ambient_light.y * scene.background_color.y,
+        scene.ambient_light.z * scene.background_color.z};
+
+    color.x += ambient_color.x;
+    color.y += ambient_color.y;
+    color.z += ambient_color.z;
+
+    for (const parser::PointLight &light : scene.point_lights) {
+        parser::Vec3f to_light = {light.position.x - pixel.x,
+                                  light.position.y - pixel.y,
+                                  light.position.z - pixel.z};
+        float distance_to_light =
+            sqrt(to_light.x * to_light.x + to_light.y * to_light.y +
+                 to_light.z * to_light.z);
+
+        if (distance_to_light > 0) {
+            to_light.x /= distance_to_light;
+            to_light.y /= distance_to_light;
+            to_light.z /= distance_to_light;
+
+            float d = dot_product(to_light, {0, 0, 1});
+
+            if (d > 0) {
+                parser::Vec3f diffuse_color = {
+                    scene.background_color.x * light.intensity.x * d,
+                    scene.background_color.y * light.intensity.y * d,
+                    scene.background_color.z * light.intensity.z * d};
+
+                color.x += diffuse_color.x;
+                color.y += diffuse_color.y;
+                color.z += diffuse_color.z;
+            }
+        }
+    }
+    color.x = std::max(0.0f, std::min(255.0f, color.x));
+    color.y = std::max(0.0f, std::min(255.0f, color.y));
+    color.z = std::max(0.0f, std::min(255.0f, color.z));
+    return color;
+}
+
+#endif
--- raytracer.cpp
+++ raytracer.cpp
@@ -1,113 +1,54 @@
 #include "Ray.h"
-
+#include "color.h"
 #include "intersect.h"
-
 #include "parser.h"
-
 #include "ppm.h"
+#include "utils.h"
 
 typedef unsigned char RGB[3];
 
-int main(int argc, char * argv[]) {
-  // Sample usage for reading an XML scene file
-  parser::Scene scene;
+int main(int argc, char *argv[]) {
+    // Sample usage for reading an XML scene file
+    parser::Scene scene;
 
-  scene.loadFromXml(argv[1]);
+    scene.loadFromXml(argv[1]);
 
-  for (parser::Camera cam: scene.cameras) {
-    int nx = cam.image_width;
-    int ny = cam.image_height;
-    unsigned char * image = new unsigned char[nx * ny * 3];
-    // x y z w => l r b t
+    for (parser::Camera cam : scene.cameras) {
+        int nx = cam.image_width;
+        int ny = cam.image_height;
+        unsigned char *image = new unsigned char[nx * ny * 3];
+        // x y z w => l r b t
 
-    int i = 0;
-    float pixel_width = (cam.near_plane.y - cam.near_plane.x) / nx;
-    float pixel_height = (cam.near_plane.w - cam.near_plane.z) / ny;
-    for (int y = 0; y < ny; ++y) {
-      for (int x = 0; x < nx; ++x) {
-        Ray r = generate_ray(cam, x, y, pixel_width, pixel_height);
-
-      //   for (parser::Sphere sphere: scene.spheres) {
-      //     float t = intersect_sphere(
-      //       scene.vertex_data[sphere.center_vertex_id - 1],
-      //       sphere.radius, r);
-
-      //     if (t > 0) {
-      //       parser::Material material =
-      //         scene.materials[sphere.material_id - 1];
-
-      //       // Calculate the intersection point
-      //       parser::Vec3f intersection_point =
-      //         add_vectors(r.get_origin(),
-      //           multiply_vector(r.get_direction(), t));
-
-      //       parser::Vec3f sphere_diffuse_color = material.diffuse;
-      //       image[i++] = static_cast < unsigned char > (
-      //         sphere_diffuse_color.x * 255);
-      //       image[i++] = static_cast < unsigned char > (
-      //         sphere_diffuse_color.y * 255);
-      //       image[i++] = static_cast < unsigned char > (
-      //         sphere_diffuse_color.z * 255);
-      //     } else {
-      //       // No intersection with the sphere, set the background
-      //       // color
-      //       image[i++] = static_cast < unsigned char > (
-      //         scene.background_color.x * 255);
-      //       image[i++] = static_cast < unsigned char > (
-      //         scene.background_color.y * 255);
-      //       image[i++] = static_cast < unsigned char > (
-      //         scene.background_color.z * 255);
-      //     }
-      //   }
-
-      //   for (parser::Triangle triangle: scene.triangles) {
-      //     const parser::Vec3f & v0 = scene.vertex_data[triangle.indices.v0_id - 1];
-      //     const parser::Vec3f & v1 = scene.vertex_data[triangle.indices.v1_id - 1];
-      //     const parser::Vec3f & v2 = scene.vertex_data[triangle.indices.v2_id - 1];
-
-      //     float t = intersect_triangle(v0, v1, v2, r);
-
-      //     if (t > 0) {
-      //       parser::Material material = scene.materials[triangle.material_id - 1];
-
-      //       // Calculate the intersection point
-      //       parser::Vec3f intersection_point =
-      //         add_vectors(r.get_origin(), multiply_vector(r.get_direction(), t));
-
-      //       parser::Vec3f triangle_diffuse_color = material.diffuse;
-      //       image[i++] = static_cast < unsigned char > (triangle_diffuse_color.x * 255);
-      //       image[i++] = static_cast < unsigned char > (triangle_diffuse_color.y * 255);
-      //       image[i++] = static_cast < unsigned char > (triangle_diffuse_color.z * 255);
-      //     } else {
-      //       // No intersection with the triangle, set the background color
-      //       image[i++] = static_cast < unsigned char > (scene.background_color.x * 255);
-      //       image[i++] = static_cast < unsigned char > (scene.background_color.y * 255);
-      //       image[i++] = static_cast < unsigned char > (scene.background_color.z * 255);
-      //     }
-      //   }
-
-        for (const parser::Mesh & mesh: scene.meshes) {
-          float t = intersect_mesh(mesh, scene.vertex_data, r);
-
-          if (t > 0) {
-            parser::Material material = scene.materials[mesh.material_id - 1];
-
-            parser::Vec3f intersection_point =
-              add_vectors(r.get_origin(), multiply_vector(r.get_direction(), t));
-
-            parser::Vec3f mesh_diffuse_color = material.diffuse;
-            image[i++] = static_cast < unsigned char > (mesh_diffuse_color.x * 255);
-            image[i++] = static_cast < unsigned char > (mesh_diffuse_color.y * 255);
-            image[i++] = static_cast < unsigned char > (mesh_diffuse_color.z * 255);
-          } else {
-            image[i++] = static_cast < unsigned char > (scene.background_color.x * 255);
-            image[i++] = static_cast < unsigned char > (scene.background_color.y * 255);
-            image[i++] = static_cast < unsigned char > (scene.background_color.z * 255);
-          }
+        int i = 0;
+        float pixel_width = (cam.near_plane.y - cam.near_plane.x) / nx;
+        float pixel_height = (cam.near_plane.w - cam.near_plane.z) / ny;
+        for (int y = 0; y < ny; ++y) {
+            for (int x = 0; x < nx; ++x) {
+                Ray r = generate_ray(cam, x, y, pixel_width, pixel_height);
+                std::vector<Intersection> intersection =
+                    intersect_objects(r, scene);
+                float min_t = std::numeric_limits<float>::max();
+                for (Intersection inter : intersection) {
+                    if (inter.t < min_t) {
+                        min_t = inter.t;
+                        parser::Vec3f color = compute_color(scene, inter);
+                        image[i] = color.x;
+                        image[i + 1] = color.y;
+                        image[i + 2] = color.z;
+                    }
+                }
+                if (min_t == std::numeric_limits<float>::max()) {
+                    parser::Vec3f pixel =
+                        add_vectors(r.get_origin(), r.get_direction());
+                    parser::Vec3f color =
+                        compute_background_color(pixel, scene);
+                    image[i] = color.x;
+                    image[i + 1] = color.y;
+                    image[i + 2] = color.z;
+                }
+                i += 3;
+            }
         }
-
-      }
+        write_ppm("simple.ppm", image, nx, ny);
     }
-    write_ppm("simple.ppm", image, nx, ny);
-  }
 }
