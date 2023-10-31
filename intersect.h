11ed1fe78e7adbcb0b86a0dc8cb5c0339189cd5f
BahadirAydin <bahadiraydin1923@gmail.com>
2023-10-31 17:34:40 +0300

    intersect_objects

--- intersect.h
+++ intersect.h
@@ -1,3 +1,6 @@
+#ifndef INTERSECT_H
+#define INTERSECT_H
+
 #include "Ray.h"
 #include "parser.h"
 #include "utils.h"
@@ -21,26 +24,22 @@ inline float intersect_sphere(const parser::Vec3f &vertex, float radius,
     float c = dot_product(subtract_vectors(origin, vertex),
                           subtract_vectors(origin, vertex)) -
               radius * radius;
-
     float delta = b * b - 4 * a * c;
-
-    // std::cout << "delta: " << delta << std::endl;
-    if (delta < 0) {
+    if (delta > -0.00001 && delta < 0.00001) {
         return -1;
     }
-
     float t1 = (-b + std::sqrt(delta)) / (2 * a);
     float t2 = (-b - std::sqrt(delta)) / (2 * a);
 
-    if (t1 < 0 && t2 < 0) {
+    if (t1 < 0.00001 && t2 < 0.00001) {
         return -1;
     }
 
-    if (t1 < 0) {
+    if (t1 > -0.00001 && t1 < 0.00001) {
         return t2;
     }
 
-    if (t2 < 0) {
+    if (t2 > -0.00001 && t2 < 0.00001) {
         return t1;
     }
 
@@ -115,7 +114,7 @@ inline std::vector<Intersection> intersect_objects(const Ray &r,
             intersection.point = add_vectors(
                 r.get_origin(), multiply_vector(r.get_direction(), t));
             parser::Vec3f normal = subtract_vectors(intersection.point, center);
-            normalize(normal);
+            normal = normalize(normal);
             intersection.normal = normal;
             intersection.material = s.materials[sphere.material_id - 1];
             intersection.t = t;
@@ -145,8 +144,31 @@ inline std::vector<Intersection> intersect_objects(const Ray &r,
             intersections.push_back(intersection);
         }
     }
-    // TODO
-    // mesh calc.
 
+    for (const parser::Mesh &mesh : s.meshes) {
+        for (const parser::Face &face : mesh.faces) {
+            parser::Vec3f vertex1 = s.vertex_data[face.v0_id - 1];
+            parser::Vec3f vertex2 = s.vertex_data[face.v1_id - 1];
+            parser::Vec3f vertex3 = s.vertex_data[face.v2_id - 1];
+
+            float t = intersect_triangle(vertex1, vertex2, vertex3, r);
+
+            if (t > 0) {
+                Intersection intersection;
+                intersection.point = add_vectors(
+                    r.get_origin(), multiply_vector(r.get_direction(), t));
+
+                parser::Vec3f edge1 = subtract_vectors(vertex2, vertex1);
+                parser::Vec3f edge2 = subtract_vectors(vertex3, vertex1);
+                parser::Vec3f cross_val = cross_product(edge1, edge2);
+                intersection.normal = normalize(cross_val);
+
+                intersection.material = s.materials[mesh.material_id - 1];
+                intersection.t = t;
+                intersections.push_back(intersection);
+            }
+        }
+    }
     return intersections;
 }
+#endif
