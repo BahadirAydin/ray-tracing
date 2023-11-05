94902bf6c1e171f6508db890c8dcff64d11c9530
DENIZCAN YILMAZ <e2444172@ceng.metu.edu.tr>
2023-11-05 20:16:21 +0300

    zort

--- color.h
+++ color.h
@@ -48,12 +48,12 @@ inline parser::Vec3f calculate_specular(float phong,
   float cos_alpha_prime = std::max(0.0f, dot_product(normal, half));
   float angle = angle_bw_vectors(to_light, half);
 
-  if (angle < 90) {
-    float b = std::pow(cos_alpha_prime, phong);
-    specular.x = material_specular.x * irradiance.x * b;
-    specular.y = material_specular.y * irradiance.y * b;
-    specular.z = material_specular.z * irradiance.z * b;
-  }
+  // if (angle < 90) {
+  float b = std::pow(cos_alpha_prime, phong);
+  specular.x = material_specular.x * irradiance.x * b;
+  specular.y = material_specular.y * irradiance.y * b;
+  specular.z = material_specular.z * irradiance.z * b;
+  // }
   return specular;
 }
 
